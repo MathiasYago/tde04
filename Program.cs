8d143e22617c6ad8149f9c16b4e309e0403b31e4
mathiasyago <mathias.santos@rede.ulbra.br>
2024-08-29 23:57:29 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿//using System.Diagnostics.CodeAnalysis;
+
+// nota = 85;
+
+// //if (NotNullAttribute >= 90)
+// {
+//     Console.whireteLine("Aprovado com exceléncia");
+// }
+// else if (NotNullAttribute >= 60)
+// {
+//     else if (NotNullAttribute >= 60)
+// }
+// {
+//     console.whireteLine("aprovado");
+// }
+// else
+// {
+// else
+// {
+//     Console.WriteLine("reprovado")
+// }
+
+// string (cor) = "vermelho";
+
+// switch (cor)
+// {
+//     case "vermelhor":
+//    Console.WriteLine("A cor é vermelhor")
+//    break;
+//    case "azul";
+//    Console.WriteLine("A cor é azul");
+//    break;
+//    default;
+//    Console.WriteLine("a cor é a azul")
+// }
+
+// for(int i =0; i <5; i++)
+// {
+//     Console.WriteLine ($"indice: {i} ");
+// }
+// int contador = 0;
+// while (contador < 5)
+// {
+//     Console.WriteLine($"Contador: {contador}");
+//     contador++;
+// }while (contador < 5);
+for (int =0 i , 10; int++)
+{
+    if  (i == 5)
+    [
+        break
+    ]
+    console.whireteLine(i);
+}
---
+++ TDE04/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+
+double totalPedido = 0;
+
+bool pedidoAberto = true;
+
+while (pedidoAberto)
+{
+    Console.WriteLine("Bem-vindo ao melhor sistema de gerenciamento de pedidos da ULBRA");
+    Console.WriteLine("1 - add item ao pedido");
+    Console.WriteLine("2 - exibir total do pedido");
+    Console.WriteLine("3 - finalizar pedido e sair");
+    string opcao = Console.ReadLine();
+
+    switch(opcao)
+    {
+        case "1":
+        Console.WriteLine("escolha um item do cardapio");
+        Console.WriteLine("1 - pizza (R$30.00)");
+        Console.WriteLine("2 - hamburguer (R$15.00)");
+        Console.WriteLine("3 - refrigerante (R$5.00)");
+        string escolha = Console.ReadLine();
+
+        if (escolha == "1")
+        {
+            Console.WriteLine("Vc add uma pizza");
+            totalPedido +=30;
+        }
+        else if(escolha == "2")
+        {
+            Console.WriteLine("vc add um hamburguer");
+            totalPedido +=15;
+        }
+        else if(escolha == "3")
+        {
+            Console.WriteLine("vc add um refri");
+            totalPedido +=5;
+        }
+        else
+        {
+            Console.WriteLine("Escolha 1 2 ou 3");
+        }
+        break;
+
+        case "2":
+        Console.WriteLine(totalPedido);
+        break;
+
+        case "3":
+        Console.WriteLine("volte smp");
+        pedidoAberto = false;
+        break;
+        default:
+        Console.WriteLine("Digite uma opção válida");
+        break;
+    }
+  
+}
